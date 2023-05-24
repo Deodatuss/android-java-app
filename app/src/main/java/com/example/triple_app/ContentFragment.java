8e56a50293e94b6e5c53d8ad027916bfe5cf3883
Deodatuss <serbgan03@gmail.com>
2023-05-25 02:06:03 +0300

    add design fragment

---
+++ app/src/main/java/com/example/triple_app/ContentFragment.java
@@ -0,0 +1,10 @@
+package com.example.triple_app;
+
+import androidx.fragment.app.Fragment;
+
+public class ContentFragment extends Fragment {
+
+    public ContentFragment(){
+        super(R.layout.fragment_content);
+    }
+}
